4ff3121d9e144b5d756bb78745862ed1f7e4bf3d
dogwith1eye <dogwith1eye@gmail.com>
2020-02-28 08:56:44 -0500

    Changed names to follow example

--- src/purescript/Counter.purs
+++ src/purescript/Counter.purs
@@ -13,11 +13,11 @@ counterCpt :: R.Component Props
 counterCpt = R.hooksComponent "Counter" cpt
   where
     cpt {} _ = do
-      y /\ setY <- R.useState' 0
+      count /\ setCount <- R.useState' 0
       pure $
         H.div {}
-          [ H.p {} [H.text ("You clicked " <> (show y) <> " times") ]
-          , H.button { type: "button", on: { click: \_ -> setY (_ + 1) } } [ H.text "Click me" ]
+          [ H.p {} [ H.text ("You clicked " <> (show count) <> " times") ]
+          , H.button { type: "button", on: { click: \_ -> setCount (_ + 1) } } [ H.text "Click me" ]
           ]
 
 counter :: Record Props -> Element
